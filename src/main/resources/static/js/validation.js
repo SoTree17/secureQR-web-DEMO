d54d39a2b99798cd7b84da37f1d4f761be867a65
ChoiJunho <ajchoi0928@gmail.com>
2021-11-05 01:30:35 +0900

    Feat: QR 생성시 Crypto Method 인자 셀렉트 박스로 받기

--- src/main/resources/static/js/validation.js
+++ src/main/resources/static/js/validation.js
@@ -6,6 +6,12 @@ function formValidCheck() {
     const width = content.width.value;
     const height = content.height.value;
 
+
+    /*const url_re = new RegExp("https?:\\/\\/[\\w\\-\\.]+/g");
+    if (!url_re.test(authURL)) {
+        alert("요청 URL 형식을 확인해주세요!");
+        return false;
+    }*/
     if (!authURL || !data || !c_index || !width || !height) {
         alert("빈칸을 채워주세요!");
         return false;
@@ -15,14 +21,15 @@ function formValidCheck() {
         return true;
     }
 }
-function formValidCheck_Register(){
+
+function formValidCheck_Register() {
     const content = document.cryptoForm;
     const authURL = content.authUrl.value;
     const token = content.token.value;
-    if(!authURL || !token){
+    if (!authURL || !token) {
         alert("저희의 QR 코드 라이브러리를 적용한 서버의 주소 또는 토큰 정보 적어주세요!");
         return false;
-    }else{
+    } else {
         alert("QR 코드 해싱 및 암호화 방식이 추가되었습니다!")
         return true;
     }
